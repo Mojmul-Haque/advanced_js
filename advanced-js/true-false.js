157cd93ad879b45b931bd0b377e2b19a27ed6262
Mojmul Haque <mojmul150@gmail.com>
2021-02-02 21:07:53 +0600

    true-false value practice done

---
+++ advanced-js/true-false.js
@@ -0,0 +1,21 @@
+const age = undefined
+    // if (age == 4) {
+    //     console.log(true)//true
+    // }
+
+// if (age) {
+//     console.log(true) true
+// }
+if (age == true) {
+    console.log(true)
+} else {
+    // console.log(false) //false 
+    // console.log(false) //false age = 0 
+    // console.log(false) //false age = null 
+    console.log(false) //false age = NaN 
+    console.log(false) //false age =undifined 
+    console.log(false) //false age ='' empty string 
+}
+
+
+// false value : null, 0, NaN, empty string, variable age = false; undifined
